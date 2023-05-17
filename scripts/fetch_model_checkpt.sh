081c6c991c3a14002773b08355179450e8bedbef
Xiaofeng Gao <xfgao@g.ucla.edu>
2023-05-17 13:36:21 -0700

    Update fetch_model_checkpt.sh

--- scripts/fetch_model_checkpt.sh
+++ scripts/fetch_model_checkpt.sh
@@ -1,5 +1,5 @@
 # fetch pretrained performer checkpoint
-aws s3 cp s3://dialfred-resource/et_augmented_human_subgoal/ ~/DialFRED/logs/pretrained/performer/ --recursive --no-sign-request
+aws s3 cp s3://dialfred-resources/et_augmented_human_subgoal/ ~/DialFRED/logs/pretrained/performer/ --recursive --no-sign-request
 
 # fetch finetuned questioner checkpoint
-aws s3 cp s3://dialfred-resource/questioner_anytime_finetuned.pt ~/DialFRED/logs/pretrained/questioner_anytime_finetuned.pt --no-sign-request
+aws s3 cp s3://dialfred-resources/questioner_anytime_finetuned.pt ~/DialFRED/logs/pretrained/questioner_anytime_finetuned.pt --no-sign-request
